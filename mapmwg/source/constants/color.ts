f0346d73f4221b4277003fa5702816b974a2e58d
nguyeh8 <nguyentran.07052002ak@gmail.com>
2023-11-03 10:01:45 +0700

    search_bar_ui

---
+++ mapmwg/source/constants/color.ts
@@ -0,0 +1,4 @@
+export const primaryColor = 'white';
+export const secondaryColor = 'blue';
+export const tertiaryColor = 'black'
+export const textColor = 'black';
---
+++ mapmwg/source/screens/Map.tsx
@@ -0,0 +1,172 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, TouchableOpacity, TextInput} from 'react-native';
+import Mapbox from '@rnmapbox/maps';
+import {primaryColor, tertiaryColor} from '../constants/color';
+import Icon from 'react-native-vector-icons/Feather';
+
+const APIKEY =
+  'pk.eyJ1Ijoibmd1eWVuaDgiLCJhIjoiY2xvZHIwaWVoMDY2MzJpb2lnOHh1OTI4MiJ9.roagibKOQ4EdGvZaPdIgqg';
+
+Mapbox.setAccessToken(APIKEY);
+Mapbox.setWellKnownTileServer('Mapbox');
+
+const MapScreen: React.FC = () => {
+  const [destination, setDestination] = useState<[number, number] | null>(null);
+  const [routeDirection, setRouteDirection] = useState<any | null>(null);
+  const [currentLocation, setCurrentLocation] = useState<[number, number]>([
+    106, 11,
+  ]);
+  const [locationLoaded, setLocationLoaded] = useState(true);
+
+  useEffect(() => {
+    if (destination) {
+      const fetchData = async () => {
+        await createRouterLine(currentLocation, destination);
+      };
+
+      fetchData(); // Call the function immediately
+
+      const interval = setInterval(fetchData, 4000); // Call the function every 4 seconds
+
+      return () => {
+        clearInterval(interval); // Clear the interval when the component unmounts
+      };
+    }
+  }, [currentLocation, destination]);
+
+  const handleUserLocationUpdate = (location: any) => {
+    const {latitude, longitude} = location.coords;
+    setCurrentLocation([longitude, latitude]);
+  };
+
+  function makeRouterFeature(coordinates: [number, number][]): any {
+    let routerFeature = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: coordinates,
+          },
+        },
+      ],
+    };
+    return routerFeature;
+  }
+
+  async function createRouterLine(
+    startCoords: [number, number],
+    endCoords: [number, number],
+  ): Promise<void> {
+    const startCoordinates = `${startCoords[0]},${startCoords[1]}`;
+    const endCoordinates = `${endCoords[0]},${endCoords[1]}`;
+    const geometries = 'geojson';
+    const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${startCoordinates};${endCoordinates}?alternatives=true&geometries=${geometries}&steps=true&banner_instructions=true&overview=full&voice_instructions=true&access_token=${APIKEY}`;
+    console.log('url: ' + url);
+
+    try {
+      let response = await fetch(url);
+      let json = await response.json();
+      let coordinates = json.routes[0].geometry.coordinates;
+
+      if (coordinates.length) {
+        const routerFeature = makeRouterFeature([...coordinates]);
+        setRouteDirection(routerFeature);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleMapPress = (event: any) => {
+    if (event.geometry) {
+      // Get location by click
+      const newDestination: [number, number] = [
+        event.geometry.coordinates[0],
+        event.geometry.coordinates[1],
+      ];
+      setDestination(newDestination);
+
+      // Create a new route
+      createRouterLine(currentLocation, newDestination);
+    }
+  };
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.page}>
+        {locationLoaded && (
+          <Mapbox.MapView
+            style={styles.map}
+            styleURL="mapbox://styles/mapbox/outdoors-v12"
+            rotateEnabled={true}
+            zoomEnabled={true}
+            onPress={handleMapPress}>
+            <Mapbox.Camera
+              centerCoordinate={currentLocation}
+              zoomLevel={15}
+              animationMode={'flyTo'}
+              animationDuration={6000}
+            />
+            {destination && (
+              <Mapbox.PointAnnotation id="marker" coordinate={destination}>
+                <View></View>
+              </Mapbox.PointAnnotation>
+            )}
+            <Mapbox.UserLocation
+              minDisplacement={1}
+              visible={true}
+              onUpdate={handleUserLocationUpdate}
+              showsUserHeadingIndicator={true}
+              androidRenderMode="gps"
+              animated={true}
+            />
+            {routeDirection && (
+              <Mapbox.ShapeSource id="line" shape={routeDirection}>
+                <Mapbox.LineLayer
+                  id="routerLine"
+                  style={{lineColor: 'blue', lineWidth: 6}}
+                />
+              </Mapbox.ShapeSource>
+            )}
+          </Mapbox.MapView>
+        )}
+      </View>
+      <View style={styles.search__bar}>
+        <Icon name="search" style={styles.search__bar_icon} size={30} color="black" />
+        <TextInput />
+      </View>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+
+  search__bar: {
+    width: '80%',
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: tertiaryColor,
+    backgroundColor: primaryColor,
+    alignSelf: 'center',
+    top: 50,
+    position: 'absolute',
+    color: tertiaryColor,
+  },
+  search__bar_icon: {
+    
+  },
+});
