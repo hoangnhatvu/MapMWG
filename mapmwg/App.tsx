4326a4ea6a0765cc3626e3f4e299f6982d2aeefc
Kha <khaaxuannguyen@gmail.com>
2023-11-07 15:05:30 +0700

    up

--- mapmwg/App.tsx
+++ mapmwg/App.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import MapScreen from './source/screens/MapScreen';
-import {StyleSheet, View} from 'react-native';
-import DirectionScreen from './source/screens/DirectionScreen';
 
 const App: React.FC = () => {
   return <MapScreen />;
