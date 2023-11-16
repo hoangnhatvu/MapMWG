420cf830e0073f429a4231126f6d880147a44b92
Kha <khaaxuannguyen@gmail.com>
2023-11-16 16:27:45 +0700

    up

--- mapmwg/source/components/InstructionSheet.tsx
+++ mapmwg/source/components/InstructionSheet.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {primaryColor, textColor} from '../constants/color';
-import { useSelector, useDispatch } from 'react-redux';
+import {primaryColor, textColor, ic_circleColor} from '../constants/color';
+import {useSelector, useDispatch} from 'react-redux';
 import RootState from '../../redux';
-import { setIsInstructed } from '../redux/slices/isInstructedSlice';
-import { setRouteDirection } from '../redux/slices/routeDirectionSlide';
-import { setDestination } from '../redux/slices/destinationSlice';
-import { setIsDirected } from '../redux/slices/isDirectedSlide';
+import {setIsInstructed} from '../redux/slices/isInstructedSlice';
+import {setRouteDirection} from '../redux/slices/routeDirectionSlide';
+import {setDestination} from '../redux/slices/destinationSlice';
+import {setIsDirected} from '../redux/slices/isDirectedSlide';
 
 interface InstructionProps {
   distance: number | null;
@@ -15,44 +15,50 @@ interface InstructionProps {
 }
 
 const InstructionSheet: React.FC<InstructionProps> = ({distance, time}) => {
-    const isInstructed = useSelector(
-        (state: RootState) => state.isInstructed.value,
-      );
-      const isDirected = useSelector(
-        (state: RootState) => state.isDirected.value,
-      );
-      const isSearch = useSelector(
-        (state: RootState) => state.isSearch.value,
-      );
-      const destination = useSelector(
-        (state: RootState) => state.destination.value,
-      );
-      const routeDirection = useSelector(
-        (state: RootState) => state.routeDirection.value,
-      );
-    
-      const dispatch = useDispatch();
+  const isInstructed = useSelector(
+    (state: RootState) => state.isInstructed.value,
+  );
+  const isDirected = useSelector((state: RootState) => state.isDirected.value);
+  const isSearch = useSelector((state: RootState) => state.isSearch.value);
+  const destination = useSelector(
+    (state: RootState) => state.destination.value,
+  );
+  const routeDirection = useSelector(
+    (state: RootState) => state.routeDirection.value,
+  );
+
+  const dispatch = useDispatch();
 
   const close = () => {
     dispatch(setIsInstructed(false));
     dispatch(setRouteDirection(null));
     dispatch(setDestination(null));
     dispatch(setIsDirected(false));
-  }  
+  };
 
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={close}>
-        <Ionicons name="close-circle-outline" size={32} color={textColor} />
+        <Ionicons
+          name="close-circle-outline"
+          size={70}
+          color={ic_circleColor}
+        />
       </TouchableOpacity>
       <View style={{flexDirection: 'column', margin: 10}}>
-        <Text style={{color:'forestgreen', fontWeight: 'bold', fontSize: 32}}>
-            {time} min
+        <Text style={{color: 'forestgreen', fontWeight: 'bold', fontSize: 32}}>
+          {time} min
         </Text>
-        <Text style={{color: 'lightgray', fontSize: 16}}>
-            {distance} km
-        </Text>
+        <Text style={{color: 'lightgray', fontSize: 16}}>{distance} km</Text>
       </View>
+      <TouchableOpacity onPress={close}>
+        <Ionicons
+          name="chevron-down-circle-outline"
+          size={70}
+          color={ic_circleColor}
+          style={styles.icon}
+        />
+      </TouchableOpacity>
       <View></View>
     </View>
   );
@@ -72,7 +78,9 @@ const styles = StyleSheet.create({
     backgroundColor: primaryColor,
     bottom: 0,
   },
-
+  icon: {
+    marginLeft: '-2%',
+  },
 });
 
 export default InstructionSheet;
--- mapmwg/source/constants/color.ts
+++ mapmwg/source/constants/color.ts
@@ -1,5 +1,6 @@
 export const primaryColor = 'white';
 export const secondaryColor = '#00BFFF';
-export const tertiaryColor = 'black'
+export const tertiaryColor = 'black';
 export const textColor = 'black';
-export const lightGray = '#d3d3d3'
+export const lightGray = '#d3d3d3';
+export const ic_circleColor = '#4E5256';
