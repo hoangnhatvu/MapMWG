e1048a6ff259ada56ebfc66e16e776ec1932469d
Xuan Khaa <khaaxuannguyen@gmail.com>
2023-11-20 09:32:05 +0700

    up

--- mapmwg/source/screens/SearchScreen.tsx
+++ mapmwg/source/screens/SearchScreen.tsx
@@ -5,10 +5,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import {
-  lightGray,
-  primaryColor,
-} from '../constants/color';
+import {lightGray, primaryColor} from '../constants/color';
 import {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {setDestination} from '../redux/slices/destinationSlice';
@@ -17,14 +14,14 @@ import {setIsSearch} from '../redux/slices/isSearchSlice';
 import {setSearchText} from '../redux/slices/searchTextSlice';
 import {searchApi} from '../services/search';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { setIsSearchCurrent } from '../redux/slices/isSearchCurrentSlice';
-import { setCurrent } from '../redux/slices/currentSlice';
-import { setIsSearchDestination } from '../redux/slices/isSearchDestinationSlice';
+import {setIsSearchCurrent} from '../redux/slices/isSearchCurrentSlice';
+import {setCurrent} from '../redux/slices/currentSlice';
+import {setIsSearchDestination} from '../redux/slices/isSearchDestinationSlice';
 import SearchBar from '../components/SearchBar';
-import { setIsDirected } from '../redux/slices/isDirectedSlide';
-import { setIsSearchBar } from '../redux/slices/isSearchBarSlice';
-import { setDestinationInfo } from '../redux/slices/destinationInfoSlice';
-import { setCurrentInfo } from '../redux/slices/currentInfoSlice';
+import {setIsDirected} from '../redux/slices/isDirectedSlide';
+import {setIsSearchBar} from '../redux/slices/isSearchBarSlice';
+import {setDestinationInfo} from '../redux/slices/destinationInfoSlice';
+import {setCurrentInfo} from '../redux/slices/currentInfoSlice';
 
 const SearchScreen = () => {
   const searchText = useSelector((state: RootState) => state.searchText.value);
@@ -53,7 +50,7 @@ const SearchScreen = () => {
   }, [searchText]);
 
   const handleSearchLocation = (location: any) => {
-    if(isSearchCurrent || isSearchDestination) {
+    if (isSearchCurrent || isSearchDestination) {
       dispatch(setIsDirected(true));
       dispatch(setIsSearchBar(false));
     }
@@ -61,55 +58,53 @@ const SearchScreen = () => {
   };
 
   const handleSearchResult = (location: any) => {
-    if(isSearch){
+    if (isSearch) {
       dispatch(setIsSearch(false));
       dispatch(setSearchText(location.properties.searchAddress));
       dispatch(setDestination(location.geometry.coordinates));
-      dispatch(setDestinationInfo(location))
-    } else if (isSearchCurrent)
-    {
+      dispatch(setDestinationInfo(location));
+    } else if (isSearchCurrent) {
       dispatch(setIsSearchCurrent(false));
       dispatch(setCurrentInfo(location));
       dispatch(setCurrent(location.geometry.coordinates));
       dispatch(setSearchText(''));
-    } else if (isSearchDestination){
+    } else if (isSearchDestination) {
       dispatch(setIsSearchDestination(false));
       dispatch(setDestinationInfo(location));
       dispatch(setDestination(location.geometry.coordinates));
       dispatch(setSearchText(''));
-    }  
-    
+    }
   };
 
   return (
     <>
-        <View style={styles.container}>
-          <View style={styles.search__container}>
-            <ScrollView>
-              {searchList?.map((location, index) => (
-                <View style={styles.menuWrapper}>
-                  <Ionicons name="location-outline" size={24} />
-                  <TouchableOpacity
-                    key={index} 
-                    style={styles.search__location}
-                    onPress={() => handleSearchLocation(location)}>
-                    <View style={{flexDirection: 'row'}}>
-                      <View>
-                        <Text
-                          numberOfLines={1}
-                          ellipsizeMode="tail"
-                          style={styles.addressText}>
-                          {location.properties.searchAddress}
-                        </Text>
-                      </View>
+      <View style={styles.container}>
+        <View style={styles.search__container}>
+          <ScrollView>
+            {searchList?.map((location, index) => (
+              <View style={styles.menuWrapper}>
+                <Ionicons name="location-outline" size={24} />
+                <TouchableOpacity
+                  key={index}
+                  style={styles.search__location}
+                  onPress={() => handleSearchLocation(location)}>
+                  <View style={{flexDirection: 'row'}}>
+                    <View>
+                      <Text
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        style={styles.addressText}>
+                        {location.properties.searchAddress}
+                      </Text>
                     </View>
-                  </TouchableOpacity>
-                </View>
-              ))}
-            </ScrollView>
-          </View>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </ScrollView>
         </View>
-        {isSearchCurrent || isSearchDestination ? <SearchBar/> : <></>}
+      </View>
+      {isSearchCurrent || isSearchDestination ? <SearchBar /> : <></>}
     </>
   );
 };
