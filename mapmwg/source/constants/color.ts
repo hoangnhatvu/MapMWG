d2edd860157bca8620a1ae2bb327542f50b088ab
Xuan Khaa <khaaxuannguyen@gmail.com>
2023-11-22 09:24:51 +0700

    update

--- mapmwg/source/constants/color.ts
+++ mapmwg/source/constants/color.ts
@@ -1,11 +1,6 @@
 export const primaryColor = 'white';
-<<<<<<< HEAD
-export const secondaryColor = '#00BFFF';
-export const tertiaryColor = 'black';
-=======
 export const secondaryColor = 'forestgreen';
-export const tertiaryColor = 'black'
->>>>>>> main
+export const tertiaryColor = 'black';
 export const textColor = 'black';
 export const lightGray = '#d3d3d3';
 export const ic_circleColor = '#4E5256';
