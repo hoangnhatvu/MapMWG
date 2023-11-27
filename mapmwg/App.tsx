56ea90ad007176b7158fffe40a0a5ee1fb91e95d
nguyeh8 <nguyentran.07052002ak@gmail.com>
2023-11-27 14:28:07 +0700

    request permission

--- mapmwg/App.tsx
+++ mapmwg/App.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Provider} from 'react-redux';
 import store from './source/redux/store';
 
 import MapScreen from './source/screens/MapScreen';
-import {View} from 'react-native';
+import {Platform} from 'react-native';
+import Mapbox from '@rnmapbox/maps';
 
 const App: React.FC = () => {
+  const [isAndroidPermissionGranted, setIsAndroidPermissionGranted] = useState<boolean>(false);
+  const [isFetchingAndroidPermission, setIsFetchingAndroidPermission] = useState<boolean>(true);
+  const IS_ANDROID = Platform.OS === 'android';
+
+  useEffect(() => {
+    if (IS_ANDROID) {
+      (async () => {
+        const isGranted = await Mapbox.requestAndroidLocationPermissions();
+        setIsAndroidPermissionGranted(isGranted);
+        setIsFetchingAndroidPermission(false);
+      })();
+    } else {
+      setIsFetchingAndroidPermission(false);
+    }
+  }, []);
+
+  if (IS_ANDROID && !isAndroidPermissionGranted) {
+    if (isFetchingAndroidPermission ) {
+      return null;
+    }
+  }
+
   return (
     <Provider store={store}>
         <MapScreen />
-
     </Provider>
   );
 };
 
 export default App;
+
