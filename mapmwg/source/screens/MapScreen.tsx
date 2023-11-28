32b153c25e0adb52ab62e7c5d80c186e1536720a
nguyeh8 <nguyentran.07052002ak@gmail.com>
2023-11-28 13:58:33 +0700

    add icon each destinations

--- mapmwg/source/screens/MapScreen.tsx
+++ mapmwg/source/screens/MapScreen.tsx
@@ -130,7 +130,6 @@ const MapScreen: React.FC = () => {
           searchDirections[1].coordinates,
         );
         dispatch(setRouteDirection(route));
-
       };
 
       fetchData();
@@ -171,6 +170,7 @@ const MapScreen: React.FC = () => {
   const handleUserLocationUpdate = (location: any) => {
     const {latitude, longitude} = location.coords;
     setCurrentLocation([longitude, latitude]);
+    dispatch(updateSearchDirection({id: 0, data: [longitude, latitude]}));
 
     let minDistance = 1;
     let newInstruction = '';
@@ -302,7 +302,6 @@ const MapScreen: React.FC = () => {
               animationDuration={initial ? 0 : 2000}
               zoomLevel={15}
               followUserMode={UserTrackingMode.FollowWithHeading}
-              
             />
           )}
           {isGuided && (
@@ -317,12 +316,16 @@ const MapScreen: React.FC = () => {
             />
           )}
 
-          {searchDirections[1].coordinates !== null && (
-            <Mapbox.PointAnnotation
-              id="destination"
-              coordinate={searchDirections[1].coordinates}>
-              <View></View>
-            </Mapbox.PointAnnotation>
+          {searchDirections.slice(1).map(
+            (direction, index) =>
+              direction.coordinates !== null && (
+                <Mapbox.PointAnnotation
+                  id={`destination_${index}`}
+                  coordinate={direction.coordinates}>
+                  <View>
+                  </View>
+                </Mapbox.PointAnnotation>
+              )
           )}
           <Mapbox.UserLocation
             minDisplacement={10}
